da7da4b62e098699884b9773feb67705aa8e02f6
PKxk8J <pkxk8j@users.noreply.github.com>
2017-10-14 22:12:28 +0900

    要らない文字削除

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http')
 const url = require('url')
 
 http.createServer(function (req, res) {
-  const query = url.parse(req.url, true).query;
+  const query = url.parse(req.url, true).query
   const title = query.title || req.url
   const body = query.body || req.url
   res.write(`<!DOCTYPE html>
